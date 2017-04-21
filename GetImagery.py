a25b3678096e6c58984080f6137453c2c4544fb4
Ivo de Liefde <Ivo de Liefde>
2017-04-21 19:12:58 +0200

    cleaned up

--- GetImagery.py
+++ GetImagery.py
@@ -3,23 +3,42 @@ from pyproj import Proj, transform
 import matplotlib.pyplot as plt
 from skimage import io
 
-# Get your coordinates according to your IP address
-coords = requests.get('https://freegeoip.net/csv/').text
-lat, lon = coords.split(',')[-3:-1]          
-lat, lon = float(lat), float(lon)
+GetMyCoords():
+	# Get your coordinates according to your IP address
+	coords = requests.get('https://freegeoip.net/csv/').text
+	lat, lon = coords.split(',')[-3:-1]          
+	lat, lon = float(lat), float(lon)
+	return lat, lon
 
-# Reproject to epsg:3035
-inProj = Proj(init='epsg:4326')
-outProj = Proj(init='epsg:3035')
-x, y = transform(inProj,outProj,lon, lat)
+ReprojectPoint(x, y, projIn, ProjOut):
+	# Reproject to epsg:3035
+	inProj = Proj(init=projIn)
+	outProj = Proj(init=ProjOut)
+	x, y = transform(inProj,outProj, x, y)
+	return x, y
 
-# Create the bounding box
-diffx = x * 0.0011318633808645777
-diffy = y * 0.0003964062069618698
-x1,y1,x2,y2 = x - diffx, y - diffy, x + diffx, y + diffy
-bbox = "bbox={0},{1},{2},{3}".format(x1,y1,x2,y2)
+GetMySatImgryURL(x,y):
+	# Create the bounding box
+	diffx = x * 0.0011318633808645777
+	diffy = y * 0.0003964062069618698
+	x1,y1,x2,y2 = x - diffx, y - diffy, x + diffx, y + diffy
+	bbox = "bbox={0},{1},{2},{3}".format(x1,y1,x2,y2)
+	url = 'http://copernicus.discomap.eea.europa.eu/arcgis/rest/services/GioLand/VeryHighResolution2012/MapServer/export?dpi=96&transparent=true&format=png8&{0}&bboxSR=3035&imageSR=3035&size=1908%2C544&f=image'.format(bbox)
+	return url
 
-# Get satellite imagery from Copernicus
-imgry = io.imread('http://copernicus.discomap.eea.europa.eu/arcgis/rest/services/GioLand/VeryHighResolution2012/MapServer/export?dpi=96&transparent=true&format=png8&{0}&bboxSR=3035&imageSR=3035&size=1908%2C544&f=image'.format(bbox))
-plt.imshow(imgry)
-plt.show()
+PlotImageFromURL(url):
+	# Get satellite imagery from Copernicus
+	imgry = io.imread(url)
+	plt.imshow(imgry)
+	plt.show()
+	return
+
+main():
+	lat,lon = GetMyCoords()
+	x,y = Reproject(lon, lat, "epsg:4326", "epsg:3035")
+	url = GetMySatImgrURL(x,y)
+	PlotImageFromURL(url)
+
+
+if __name__ == '__main__':
+	main()
