3ea8e12034cf47de46a802844279bff4bec4345f
= <=>
2017-04-26 09:00:41 +0200

    read image as numpy array

--- GetImagery.py
+++ GetImagery.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from skimage import io
 import georasters as gr
 import pandas as pd
+from scipy import ndimage
+from io import BytesIO
 
 
 def GetMyCoords():
@@ -34,46 +36,51 @@ def PlotImageFromURL(url):
 	# Load image from url
 	imgry = io.imread(url)
 	plt.imshow(imgry)
+	plt.axis("off")
 	plt.show()
 	return
 
-def img2df(url):
-	# raster = io.imread(url)
-	data = gr.from_file(url)
-	df = data.to_pandas()
-	return df
+# def img2df(url):
+	# # raster = io.imread(url)
+	# data = gr.from_file(url)
+	# df = data.to_pandas()
+	# return df
 
-def dfKernel(df, size, function):
-	if size % 2 == 0:
-		print("Only uneven kernel size is allowed")
-		return 
-
-	Xmax = int(max(df['x'])) - (size-1)//2
-	Ymax = int(max(df['y'])) - (size-1)//2
-	df.set_index('y')
-	output = pd.DataFrame(columns=['x','y','value'])
-	for y in range(Ymax):
-		rows = df.loc[df['y'].isin(range(y, y+size))]
-		for x in range(Xmax):
-			kernel = rows.loc[df['x'].isin(range(x, x+size))]
-			print(kernel)
-			output['x'] = x
-			output['y'] = y 
-			output['value'] = function(kernel)
-	return
-
-def avg(lst):
-	pass
+# def dfKernel(df, size, function):
+	# if size % 2 == 0:
+		# print("Only uneven kernel size is allowed")
+		# return 
+	# print("don't use this, it's really slow...")
+	# Xmax = int(max(df['x'])) - (size-1)//2
+	# Ymax = int(max(df['y'])) - (size-1)//2
+	# df.set_index('y')
+	# output = pd.DataFrame(columns=['x','y','value'])
+	# for y in range(Ymax):
+		# rows = df.loc[df['y'].isin(range(y, y+size))]
+		# for x in range(Xmax):
+			# kernel = rows.loc[df['x'].isin(range(x, x+size))]
+			# print(kernel)
+			# output['x'] = x
+			# output['y'] = y 
+			# output['value'] = function(kernel)
+	# return
+	
+def imgry2array(url):
+	file = BytesIO()
+	r = requests.get(url, stream=True)
+	for chunk in r.iter_content(1024):
+		file.write(chunk)
+	imgry = ndimage.imread(file, mode='RGB')
+	print(type(imgry))
 	return 
-
+	
 def main():
 	lat,lon = GetMyCoords()
 	x,y = ReprojectPoint(lon, lat, "epsg:4326", "epsg:3035")
 	url = GetMySatImgryURL(x,y)
-	df = img2df(url)
-	# print(df.head)
-	# PlotImageFromURL(url)
-	dfKernel(df,3,avg)
+	PlotImageFromURL(url)
+	imgry2array(url)
+	
 	return
 
 if __name__ == '__main__':
